33e0671b7a3fbff808f2ad44e967c1333f5c31ac
Marlon <marlonhcooper2@gmail.com>
2023-02-05 00:38:51 -0500

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const array = Array.from(string);
 
+    let arraySwapped = [];
+    for (i = array.length -1; i >= 0; i--) {
+        arraySwapped.push(array[i]);
+    }
+    let stringSwapped = arraySwapped.join("");
+    return stringSwapped;
 };
+
+reverseString("Hello");
 
 // Do not edit below this line
 module.exports = reverseString;
