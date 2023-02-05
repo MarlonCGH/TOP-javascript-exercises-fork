23c700861c017a7a9785e01ac3c14838c4cfe660
Marlon <marlonhcooper2@gmail.com>
2023-02-04 23:43:50 -0500

    Complete exercise 1

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,7 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    return string * num
 };
+repeatString("hey", 3);
 
 // Do not edit below this line
 module.exports = repeatString;
