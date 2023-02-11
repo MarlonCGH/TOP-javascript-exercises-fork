bfca1278b04650671e63b3d57a5ca6329ad5b92a
Marlon <marlonhcooper2@gmail.com>
2023-02-11 15:41:29 -0500

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(firstNum, lastNum) {
+    let finalSum = 0;
+    if (firstNum < 0 || typeof firstNum !== "number" || lastNum < 0 || typeof lastNum !== "number"){
+        return "ERROR";
+    }
+    else if (firstNum > lastNum) {
+        for (i = lastNum; i <= firstNum; i++) {
+            finalSum += i;
+        }    
+    }
+    else {
+        for (i = firstNum; i <= lastNum; i++) {
+            finalSum += i;
+        }
+    }
 
+    return finalSum;
 };
 
 // Do not edit below this line
